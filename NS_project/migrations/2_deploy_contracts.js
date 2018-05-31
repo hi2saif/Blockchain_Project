cd7bac32cb2bbf2c701fed1a5f3be8c254046bc4
Saif Ahmed <hi2saif@gmail.com>
2018-05-31 14:33:51 +0530

    Added

--- NS_project/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Renting = artifacts.require("./Renting.sol");
-var Notary = artifacts.require("./Notary.sol");
-module.exports = function(deployer) {
-deployer.deploy(Notary);
-  deployer.deploy(Renting);
-
-}
